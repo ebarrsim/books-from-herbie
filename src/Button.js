3e093dda03f5ab06af2551675802d2177a9d8c76
ebarrsim <72933481+ebarrsim@users.noreply.github.com>
2021-03-17 15:29:33 +0000

    Added button and logo to header

---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Button";
+
+export default function Button() {
+    return(
+        <div>
+            <button type="button" className="btn btn-primary">
+            Donate
+        </button>   
+     </div>
+    );
+}
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Header.css";
 
+
 export default function Header() {
     return ( <div className="Header">
+        <img src="images/bfh-logo.png" alt="" />
         <p>
    PROVIDING RACIALLY INCLUSIVE BOOKS FOR SCHOOLS
         </p>
+        <button type="button" class="btn btn-primary">
+            Donate
+        </button>
     </div>
     );
 }
