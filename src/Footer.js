2963951b8bf1cf5cedddbfa4d0f4cc70259aa990
ebarrsim <72933481+ebarrsim@users.noreply.github.com>
2021-03-19 14:28:45 +0000

    Added Footer.js file

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+ return (
+     <div className="Footer">
+         <h1>
+             Hello
+         </h1>
+     </div>
+ );
+}
